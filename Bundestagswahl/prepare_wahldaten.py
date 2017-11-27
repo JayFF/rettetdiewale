7304b862b3b55e96244c13d5f73a9df72610a2ef
Jonathan Förste <tv@in-hd.de>
2017-11-27 16:07:02 +0100

    minor changes

--- Bundestagswahl/prepare_wahldaten.py
+++ Bundestagswahl/prepare_wahldaten.py
@@ -6,8 +6,9 @@ def strukturdaten(filename, seperator=";"):
     data = pd.read_csv(filename, seperator, encoding='latin1', skiprows=[0,1,2,3,4,5,6,7])
     return(data)
 
-def wahlergebnis(filename, seperator=";"):
-    data = pd.read_csv("btw17_kerg.csv",";",skiprows=[0,1,2,3,4])
+def wahlergebnis_recent(filename, seperator=";"):
+    '''suitable to load the sheets of btw 13 and 17'''
+    data = pd.read_csv(filename ,";", skiprows=[0,1,2,3,4])
     data2 = data.drop([column for column in list(data) if (
                         re.search("Unnamed:.*", column) and "Zweitstimmen" not in list(data[column]))],
                                                                                                 axis=1)
@@ -15,8 +16,8 @@ def wahlergebnis(filename, seperator=";"):
     for index, column in enumerate(list(data2)):
         if re.match("Unnamed: .*", column):
             #print((str(list(data)[index])+" Zweitstimmen"+str(index)))
-            data2 = data2.rename(columns={column: (str(list(data2)[index-1])+"Zweitstimmen")})
-            data2 = data2.rename(columns={list(data2)[index-1]: (str(list(data2)[index-1])+"Erststimmen")})
+            data2 = data2.rename(columns={column: (str(list(data2)[index-1])+" Zweitstimmen")})
+            data2 = data2.rename(columns={list(data2)[index-1]: (str(list(data2)[index-1])+" Erststimmen")})
     
     data2.drop(data2.index[0], inplace=True)
     data2 = data2.reset_index()
